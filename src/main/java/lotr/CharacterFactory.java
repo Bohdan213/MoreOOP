245689a4aacc6488f19879e15cacf3f2a94de138
Bohdan213 <bogdanpeleh213@gmail.com>
2022-10-13 18:44:29 +0300

    push 3.0

--- src/main/java/lotr/CharacterFactory.java
+++ src/main/java/lotr/CharacterFactory.java
@@ -9,19 +9,13 @@ import static org.reflections.scanners.Scanners.SubTypes;
 
 public class CharacterFactory {
     public static Character createCharacter() {
-        Reflections reflections = new Reflections("lotr");
+        Reflections reflections = new Reflections(Character.class.getPackage().getName());
         Set<Class<?>> factory = reflections.get(SubTypes.of(Character.class).asClass());
         factory.remove(Noble.class);
         int number = new Random().nextInt(4);
         try {
-            return (Character) factory.toArray(new Class[0])[number].getConstructor().newInstance();
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
+            return (lotr.Character) factory.toArray(new Class[0])[number].getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
     }
