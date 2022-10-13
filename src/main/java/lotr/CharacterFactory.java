d2f8b3300e19ceed0ad70d7dc5a41604bdd52b23
Bohdan213 <bogdanpeleh213@gmail.com>
2022-10-13 18:40:18 +0300

    push 2.0

--- src/main/java/lotr/CharacterFactory.java
+++ src/main/java/lotr/CharacterFactory.java
@@ -1,18 +1,28 @@
 package lotr;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
+import java.util.Set;
+
+import org.reflections.Reflections;
+
+import static org.reflections.scanners.Scanners.SubTypes;
 
 public class CharacterFactory {
     public static Character createCharacter() {
+        Reflections reflections = new Reflections("lotr");
+        Set<Class<?>> factory = reflections.get(SubTypes.of(Character.class).asClass());
+        factory.remove(Noble.class);
         int number = new Random().nextInt(4);
-        if (number == 0) {
-            return new Hobbit();
+        try {
+            return (Character) factory.toArray(new Class[0])[number].getConstructor().newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
-        if (number == 1) {
-            return new Elf();
-        }
-        if (number == 2) {
-            return new King();
-        }
-        return new Knight();
     }
 }
